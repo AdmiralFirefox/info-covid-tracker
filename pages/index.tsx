6781aa93553ce92ed79ec3ddea4537630d697bed
AdmiralFirefox <jjlcuadradev@gmail.com>
2022-11-23 16:44:33 +0800

    Cleaned Code on index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,19 @@
 import { NextPage } from "next";
 import WebHeader from "../components/homepage/WebHeader";
 import GlobalInfo from "../components/homepage/GlobalInfo";
-<<<<<<< HEAD
 import CountryTable from "../components/homepage/CountryTable";
-=======
->>>>>>> 401111852b8208539ed1331e24428b6b6178f623
 import { CountriesProps } from "../types/CountriesType";
 import globalstyles from "../components/globalstyles/globalstyles";
 
 const Home: NextPage<CountriesProps> = ({ countries }) => {
   const globalInfo = countries.Global;
-<<<<<<< HEAD
   const countriesInfo = countries.Countries;
-=======
->>>>>>> 401111852b8208539ed1331e24428b6b6178f623
-
+  
   return (
     <>
       <WebHeader />
       <GlobalInfo globalInfoData={globalInfo} />
-<<<<<<< HEAD
       <CountryTable countriesInfoData={countriesInfo} />
-=======
->>>>>>> 401111852b8208539ed1331e24428b6b6178f623
-
       <style jsx global>
         {globalstyles}
       </style>
